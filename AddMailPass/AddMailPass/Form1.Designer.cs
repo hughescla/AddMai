86849f1e9b8bd0f4d3c5e0108a15010d43941587
hughescla <131156347+hughescla@users.noreply.github.com>
2023-04-18 13:41:44 +0300

    Add files via upload

---
+++ AddMailPass/AddMailPass/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace AddMailPass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.od = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // od
+            // 
+            this.od.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 174);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(783, 191);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog od;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ AddMailPass/AddMailPass/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddMailPass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            od.InitialDirectory = Environment.CurrentDirectory;
+            if (od.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            List<string> rezultf = new List<string>();
+            List<string> infile = new List<string>(File.ReadAllLines(od.FileName));
+            od.InitialDirectory = Environment.CurrentDirectory;
+            if (od.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            List<string> mailfile = new List<string>(File.ReadAllLines(od.FileName));
+            foreach (var item in infile)
+            {
+                string mail = item.Split(';')[0];
+                string mailpasspoop3 = item.Split('|')[3];
+                richTextBox1.AppendText(mail+Environment.NewLine);
+                richTextBox1.AppendText(mailpasspoop3 + Environment.NewLine);
+                foreach (var fullmail in mailfile)
+                {
+                    if (fullmail.Split(':')[0]==mail)
+                    {
+                        string mailpassweb = fullmail.Split(':')[1];
+                        rezultf.Add(item.Replace(mailpasspoop3,mailpassweb+'|'+mailpasspoop3));
+                        break;
+                    }
+                }
+                //return;
+            }
+            File.WriteAllLines("result.txt",rezultf);
+        }
+    }
+}
